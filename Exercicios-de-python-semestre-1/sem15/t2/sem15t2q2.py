2fd8fd1a42cb9de6f6918dd0fc7c4c96edbd7602
Symon Mendes <symon28400@gmail.com>
2023-12-18 21:12:34 -0300

    Update sem15t2q2.py

--- Exercicios-de-python-semestre-1/sem15/t2/sem15t2q2.py
+++ Exercicios-de-python-semestre-1/sem15/t2/sem15t2q2.py
@@ -4,12 +4,54 @@ temperaturas acima da média anual e em que mês elas ocorreram (mostrar o mês 
 Fevereiro, ... )'''
 
 def calc():
-    matriz = []
+    anos = []
+    temperaturas = []
+    meses = [
+        "Janeiro: "
+        "Fevereiro: "
+        "Março: "
+        "Abril: "
+        "Maio: "
+        "Junho: "
+        "Agosto: "
+        "Setembro: "
+        "Outubro: "
+        "Novembro: "
+        "Dezembro: "
+    ]
 
-    for lin in range(12):
-        lin = []
+    for mes in range(3):
+        mes = []
         for col in range(1):
-            x = int(input())
-            lin.append(x)
+            temp = int(input())
+            esc = str(input().upper())
+            mes.append(temp, esc)
+            if esc == "F":
+                temp = (temp + 459.67) * 5/9
+            elif esc == "C":
+                temp = temp + 273.15
 
-        matriz.append(lin)
+        anos.append(mes)
+        temperaturas.append(temp)
+
+    
+    media = sum(temperaturas)/len(temperaturas)
+
+    temps_acima_média = []
+
+    for i in temperaturas:
+        if i > media:
+            ind = temperaturas.index(i)
+            temps_acima_média.append(meses[ind]+temperaturas[ind]+"K")
+
+    print("TEMPERATURA MÉDIA ANUAL")
+    print(media,"K")
+    print("TEMPERATURAS ACIMA DA MÉDIA ANUAL:")
+    for i in temps_acima_média:
+        print(f'{i}\n')
+
+def main():
+    calc()
+
+if __name__=="__main__":
+    main()
